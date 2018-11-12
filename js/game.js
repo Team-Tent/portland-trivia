6f59851f7c094b98bafd705aef0e3469e879aa90
Lise Brackbill <menagerie@hevanet.com>
2018-11-12 15:36:37 -0800

    can click on table cells

--- js/game.js
+++ js/game.js
@@ -1,6 +1,11 @@
 import highscoreApi from '../api/highscore-api.js';
 import Round from '../js/questions.js';
+import html from '../js/html.js';
 
 const round = new Round;
 const root = document.getElementById('root');
 root.appendChild(round.render());
+root.appendChild(html`<p>hello</p>`);
+root.addEventListener('click', function(event) {
+    console.log('clicked', event);
+});
