9fa5e6ea6bd178c2a3d917b98653b99c3243da8a
Lise Brackbill <menagerie@hevanet.com>
2018-11-12 15:25:47 -0800

    round shows on dom

--- js/game.js
+++ js/game.js
@@ -1,1 +1,6 @@
 import highscoreApi from '../api/highscore-api.js';
+import Round from '../js/questions.js';
+
+const round = new Round;
+const root = document.getElementById('root');
+root.appendChild(round.render());
--- js/questions.js
+++ js/questions.js
@@ -13,15 +13,37 @@ function makeTemplate() {
                 <th>Culture</th>
                 <th>Social</th>
             </tr>
+            <tr>
+                <td class="landmarks">$100</td>
+                <td class="culture">$100</td>
+                <td class="social">$100</td>
+            </tr>
+            <tr>
+                <td class="landmarks">$200</td>
+                <td class="culture">$200</td>
+                <td class="social">$200</td>
+            </tr>
+            <tr>
+                <td class="landmarks">$300</td>
+                <td class="culture">$300</td>
+                <td class="social">$300</td>
+            </tr>
         </table>
     `;
 }
+
+
+
 
 class Round {
     constructor(){
         this.questions = questions;
         
         // call get random function for easy, medium and hard
+    }
+    render() {
+        const dom = makeTemplate();
+        return dom;
     }
 }
 
