f8e67dbd6b6a3996ad68db7d075d4e6f1de1562c
Tyler Corbett <thetylercorbett@gmail.com>
2018-11-13 09:11:53 -0800

    started work on navbar for each page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 import highscoreApi from '../api/highscore-api.js';
+import Header from './header.js';
+
 
 const form = document.getElementById('userInfo');
+const headerSection = document.getElementById('header');
+
+
+const header = new Header;
+headerSection.appendChild(header.render());
+
 
 form.addEventListener('submit', event => {
     event.preventDefault();
